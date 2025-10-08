5990c8fc691e5876b6d75c7ffa568a359346a563
Esteban Gonzalez Iakl <1207326+Iakl@users.noreply.github.com>
2025-10-08 15:05:16 +0200

    updated dependencies

--- iaklogger/__init__.py
+++ iaklogger/__init__.py
@@ -1,4 +1,4 @@
-from .main import log, set_options, register_self, OPTIONS, log_progress, log_error, log_warning, ignore_warnings, mute_tag, unmute_tag, restore_warnings
+from .main import log, set_options, register_self, OPTIONS, log_progress, log_error, log_warning, ignore_warnings, mute_tag, unmute_tag, restore_warnings, prep_tag, unprep_tag
 
 __all__ = ["log", "set_options", "register_self",
-           "OPTIONS", "log_progress", "log_error", "log_warning", "ignore_warnings", "mute_tag", "unmute_tag", "restore_warnings"]
+           "OPTIONS", "log_progress", "log_error", "log_warning", "ignore_warnings", "mute_tag", "unmute_tag", "restore_warnings", "prep_tag", "unprep_tag"]
